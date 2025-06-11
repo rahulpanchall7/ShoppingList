f339c0491b8711ef5305d7e39ff77af43eaf94d3
rahulpanchall7 <rahulpanchal7.de@gmail.com>
2025-06-11 15:31:12 +0200

    added required dependencies and google maps api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,16 @@ android {
 
 dependencies {
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.maps.android:maps-compose:2.15.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
